5716ffdce3a96c98b1be7248310e25c3fa03b9b0
tomohiro <eggo.world@gmail.com>
2019-05-27 03:28:53 +0900

    add logistic func
    add logistic func

--- plot.py
+++ plot.py
@@ -1,34 +1,60 @@
+"""
+https://python.atelierkobato.com/sigmoid/
+"""
+from pandas import DataFrame
+from sklearn import datasets
+from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+#from sklearn.model_selection import StratifiedKFold
+from sklearn.model_selection import cross_validate
+
 import matplotlib.pyplot as plt
 import numpy as np
 import math
 
 
 def logistic_func(a,b,c,t):
-    f = 1/(1+np.exp(-1*b*(c-t)))
+    f = a / (1 + np.exp(-b * a * (t - c)))
+    #f = (a*b)/(a+(b-a)*np.exp(-1*c*t))
     return f
 
 def main():
-    
-    print("matplotlib")
-    x = np.arange(-6,6,0.1)
-    a = [-4,-2, 0, 2, 4]
-    #a = [ 1, 1, 1, 1, 1, 1, 1]
-    #a = [0,2,4,6]
-    #b = [-4,-2, 0, 2, 4]
-    b = [ -1, 1, 1, 1, 1, 1, 1]
-    #c=  [-6,-4,-2, 0, 2, 4, 6]
-    c = [ 1, 1, 1, 1, 1]
+    #clf = LogisticRegression()
 
+
+    print("matplotlib")
+    x = np.arange(-15,15,1)
+    #a = [-4,2, 4]
+    a = [ -1]#yseppen ywidth
+    b = [ 1]#傾き           constant
+    #b = [-4,-2,2, 4]
+    #c =  [-6,-4,-2, 0, 2, 4, 6]
+    #c = [-4,-2,2, 4]
+    c = [0]#xseppen  constant
+    #c = [-4,0,4]#立ち上がり中心
     #for db in b:
+    
     
     for da in a:
         for db in b:
             for dc in c:
                 f = []
-                for t in x:
+                for t in x:            
                     y=logistic_func(da,db,dc,t)
-                    f.append(y)
-                plt.plot(x,f)
+                    f.append([y,1])
+                plt.scatter(x,f,marker="o",color='red')
+        
+    X = DataFrame(x)
+    F = DataFrame(f)
+    AX,PX,AF,PF = train_test_split(X, F, test_size = 0.2, random_state = 42) # 100%のデータを学習データに、20%を検証データにする
+    #lr = LinearRegression()
+    clf = LogisticRegression()
+    #lr.fit(X,F)
+    clf.fit(AX,AF)
+    #PF = lr.predict(X)
+    PF = clf.predict(AX)
+    plt.plot(PX,PF,color='blue')
     plt.show()
 
 if __name__ == "__main__":
--- tk_mind.py
+++ tk_mind.py
@@ -9,17 +9,24 @@ center_x = bd_width/2
 center_y = bd_height/2
 dic = {}
 
+
 class Mindobj():
     md_dict={}
     posx=0
     posy=0
-    def __init__(self,name):
+    def __init__(self,name,x,y):
         # super(Mindobj,self).__init__(md_dict)
+        self.posx = x
+        self.posy = y
         self.name = tkinter.StringVar()
         self.name.set(name) 
         self.lbl=tkinter.Label(textvariable = self.name)
         self.lbl.bind('<Button-1>',self.mindobj_print)
         self.lbl.bind('<Double-Button-1>',self.edit_label)
+        self.set_position()
+
+    def set_position(self):
+        self.lbl.place( x = self.posx ,y = self.posy )
 
     def mindobj_print(self,event):
         print("click mind obj")
@@ -54,9 +61,14 @@ class Mindobj():
             for i in range(len(self.md_dict["child"])):
                 cy = self.posy + i*25
                 cx = self.posy + 100
-                mdo = Mindobj(self.md_dict["child"][str(i)]["parent"])
+                mdo = Mindobj(self.md_dict["child"][str(i)]["parent"],cx,cy)
+                #self.md_dict["child"][str(i)].setdefault("object_id",mdo)
                 mdo.md_dict = self.md_dict["child"][str(i)]
-                mdo.lbl.place( x = cx ,y = cy )
+                mdo.md_dict.setdefault("id_parent",self)
+                mdo.md_dict.setdefault("id_object",mdo)
+                canvas.create_line(self.posx + self.lbl.winfo_width(),self.posy+ self.lbl.winfo_height(),cx,cy+mdo.lbl.winfo_height(),fill="blue", width=1, smooth=True) 
+                #pprint.pprint(mdo.md_dict)
+                #mdo.lbl.place( x = cx ,y = cy )
 
 
 class Mind():
@@ -71,8 +83,8 @@ class Mind():
 
     def mind_print(self):#読み込んだjsonを表示
         print("hello my mind world!!")
-        pprint.pprint(self.mind_dic)
-        print(len(self.mind_dic["0"]["child"]))
+        #pprint.pprint(self.mind_dic)
+        #print(len(self.mind_dic["0"]["child"]))
 
 def set_Mindobj(dict):
     mind_dic = dict
@@ -94,22 +106,29 @@ def set_Mindobj(dict):
             print(child_mind_dic[str(k)])
             set_Mindobj(child_mind_dic[str(k)]["child"])
     """
-    mdo = Mindobj(mind_dic[str(i)]["parent"])
-    mdo.posy = center_y + i * 25
-    mdo.posx = center_x + j
+    cy = center_y + i * 25
+    cx = center_x + j
+    mdo = Mindobj(mind_dic[str(i)]["parent"],cx,cy)
     mdo.md_dict = mind_dic[str(i)]
-    mdo.child_print()
-    mdo.lbl.place( x = mdo.posx ,y = mdo.posy )
+    mdo.md_dict.setdefault("id_object",mdo)
+    #mdo.child_print()
+    #pprint.pprint(mdo.md_dict)
+    #mdo.lbl.place( x = mdo.posx ,y = mdo.posy )
     mdo.birth_child()
+    pprint.pprint(mind_dic)
 
 
 def main():
     global dic
+    global canvas
     root = tkinter.Tk()
     root.title("tkinter test")
+    canvas = tkinter.Canvas( width = bd_width, height = bd_height)
     root.geometry( str(bd_width) + "x" + str(bd_height))
-    canvas = tkinter.Canvas(root, width = bd_width, height = bd_height)
+    
     set_Mindobj(dic)
+    canvas.pack()#canvas set object
+    
     root.mainloop()
 
 if __name__ == "__main__":
